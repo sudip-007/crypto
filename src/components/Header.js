0551dc0760ea7406d6a3972b03e478d23bd0bc71
Sudip Kumar Shaw <51938451+sudip-007@users.noreply.github.com>
2022-01-14 21:09:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Buzz
+              Crypto Hunter
             </Typography>
             <Select
               variant="outlined"
